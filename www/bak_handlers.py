76b24a4bc8a0d6f76cf406cf0b3bdb6421817b3f
superzrr <superzrr@gmail.com>
2021-01-06 12:10:09 +0800

    day 4 one problem left

--- www/bak_handlers.py
+++ www/bak_handlers.py
@@ -6,13 +6,11 @@ __author__ = 'Michael Liao'
 ' url handlers '
 
 import re, time, json, logging, hashlib, base64, asyncio
-
 import markdown2
-
 from aiohttp import web
 
-from coroweb import get, post
-from apis import Page, APIValueError, APIResourceNotFoundError
+from vcoroweb import get, post
+from apis import Page,APIValueError,APIResourceNotFoundError
 
 from models import User, Comment, Blog, next_id
 from config import configs
@@ -44,19 +42,13 @@ def user2cookie(user, max_age):
     L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
     return '-'.join(L)
 
-def text2html(text):
-    lines = map(lambda s: '<p>%s</p>' % s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;'), filter(lambda s: s.strip() != '', text.split('\n')))
-    return ''.join(lines)
-
 @asyncio.coroutine
 def cookie2user(cookie_str):
-    '''
-    Parse cookie and load user if cookie is valid.
-    '''
     if not cookie_str:
         return None
     try:
         L = cookie_str.split('-')
+        print("#######################")
         if len(L) != 3:
             return None
         uid, expires, sha1 = L
@@ -65,6 +57,7 @@ def cookie2user(cookie_str):
         user = yield from User.find(uid)
         if user is None:
             return None
+        print(type(user))
         s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
         if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
             logging.info('invalid sha1')
@@ -75,38 +68,37 @@ def cookie2user(cookie_str):
         logging.exception(e)
         return None
 
+#@get('/')
+#async def user_index(request):
+#    users = await User.findAll()
+#    return {
+#        '__template__': 'test.html',
+#        'users': users
+#    }
+
 @get('/')
-@asyncio.coroutine
-def index(*, page='1'):
-    page_index = get_page_index(page)
-    num = yield from Blog.findNumber('count(id)')
-    page = Page(num)
-    if num == 0:
-        blogs = []
-    else:
-        blogs = yield from Blog.findAll(orderBy='created_at desc', limit=(page.offset, page.limit))
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
         '__template__': 'blogs.html',
-        'page': page,
         'blogs': blogs
     }
 
-@get('/blog/{id}')
-@asyncio.coroutine
-def get_blog(id):
-    blog = yield from Blog.find(id)
-    comments = yield from Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
-    for c in comments:
-        c.html_content = text2html(c.content)
-    blog.html_content = markdown2.markdown(blog.content)
-    return {
-        '__template__': 'blog.html',
-        'blog': blog,
-        'comments': comments
-    }
+@get('/api/users')
+def api_get_users():
+    users = yield from User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
 
 @get('/register')
 def register():
+    print("I was invoked!!!")
     return {
         '__template__': 'register.html'
     }
@@ -128,6 +120,7 @@ def authenticate(*, email, passwd):
     if len(users) == 0:
         raise APIValueError('email', 'Email not exist.')
     user = users[0]
+    logging.info("***************:",user.email)
     # check passwd:
     sha1 = hashlib.sha1()
     sha1.update(user.id.encode('utf-8'))
@@ -151,17 +144,6 @@ def signout(request):
     logging.info('user signed out.')
     return r
 
-@get('/manage/')
-def manage():
-    return 'redirect:/manage/comments'
-
-@get('/manage/comments')
-def manage_comments(*, page='1'):
-    return {
-        '__template__': 'manage_comments.html',
-        'page_index': get_page_index(page)
-    }
-
 @get('/manage/blogs')
 def manage_blogs(*, page='1'):
     return {
@@ -177,76 +159,13 @@ def manage_create_blog():
         'action': '/api/blogs'
     }
 
-@get('/manage/blogs/edit')
-def manage_edit_blog(*, id):
-    return {
-        '__template__': 'manage_blog_edit.html',
-        'id': id,
-        'action': '/api/blogs/%s' % id
-    }
-
-@get('/manage/users')
-def manage_users(*, page='1'):
-    return {
-        '__template__': 'manage_users.html',
-        'page_index': get_page_index(page)
-    }
-
-@get('/api/comments')
-@asyncio.coroutine
-def api_comments(*, page='1'):
-    page_index = get_page_index(page)
-    num = yield from Comment.findNumber('count(id)')
-    p = Page(num, page_index)
-    if num == 0:
-        return dict(page=p, comments=())
-    comments = yield from Comment.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
-    return dict(page=p, comments=comments)
-
-@post('/api/blogs/{id}/comments')
-@asyncio.coroutine
-def api_create_comment(id, request, *, content):
-    user = request.__user__
-    if user is None:
-        raise APIPermissionError('Please signin first.')
-    if not content or not content.strip():
-        raise APIValueError('content')
-    blog = yield from Blog.find(id)
-    if blog is None:
-        raise APIResourceNotFoundError('Blog')
-    comment = Comment(blog_id=blog.id, user_id=user.id, user_name=user.name, user_image=user.image, content=content.strip())
-    yield from comment.save()
-    return comment
-
-@post('/api/comments/{id}/delete')
-@asyncio.coroutine
-def api_delete_comments(id, request):
-    check_admin(request)
-    c = yield from Comment.find(id)
-    if c is None:
-        raise APIResourceNotFoundError('Comment')
-    yield from c.remove()
-    return dict(id=id)
-
-@get('/api/users')
-@asyncio.coroutine
-def api_get_users(*, page='1'):
-    page_index = get_page_index(page)
-    num = yield from User.findNumber('count(id)')
-    p = Page(num, page_index)
-    if num == 0:
-        return dict(page=p, users=())
-    users = yield from User.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
-    for u in users:
-        u.passwd = '******'
-    return dict(page=p, users=users)
-
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
 @post('/api/users')
 @asyncio.coroutine
 def api_register_user(*, email, name, passwd):
+    print("data submitting...")
     if not name or not name.strip():
         raise APIValueError('name')
     if not email or not _RE_EMAIL.match(email):
@@ -271,6 +190,7 @@ def api_register_user(*, email, name, passwd):
 @get('/api/blogs')
 @asyncio.coroutine
 def api_blogs(*, page='1'):
+    print('blog post was invoked***')
     page_index = get_page_index(page)
     num = yield from Blog.findNumber('count(id)')
     p = Page(num, page_index)
@@ -288,6 +208,7 @@ def api_get_blog(*, id):
 @post('/api/blogs')
 @asyncio.coroutine
 def api_create_blog(request, *, name, summary, content):
+    print('blog post was invoked***')
     check_admin(request)
     if not name or not name.strip():
         raise APIValueError('name', 'name cannot be empty.')
@@ -298,28 +219,3 @@ def api_create_blog(request, *, name, summary, content):
     blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image, name=name.strip(), summary=summary.strip(), content=content.strip())
     yield from blog.save()
     return blog
-
-@post('/api/blogs/{id}')
-@asyncio.coroutine
-def api_update_blog(id, request, *, name, summary, content):
-    check_admin(request)
-    blog = yield from Blog.find(id)
-    if not name or not name.strip():
-        raise APIValueError('name', 'name cannot be empty.')
-    if not summary or not summary.strip():
-        raise APIValueError('summary', 'summary cannot be empty.')
-    if not content or not content.strip():
-        raise APIValueError('content', 'content cannot be empty.')
-    blog.name = name.strip()
-    blog.summary = summary.strip()
-    blog.content = content.strip()
-    yield from blog.update()
-    return blog
-
-@post('/api/blogs/{id}/delete')
-@asyncio.coroutine
-def api_delete_blog(request, *, id):
-    check_admin(request)
-    blog = yield from Blog.find(id)
-    yield from blog.remove()
-    return dict(id=id)
